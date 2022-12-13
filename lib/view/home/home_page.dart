db5129c6d1c2cb9268166addb44e79d23ce02e47
safa <safabasheerahamed@gmail.com>
2022-12-13 13:45:25 +0530

    carousal_corrected

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -18,13 +18,15 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final homeProvider = Provider.of<HomeController>(context, listen: false);
     final providerWish =
         Provider.of<ScreenWishlistProvider>(context, listen: false);
+    final providerhome = Provider.of<HomeController>(context, listen: false);
     WidgetsBinding.instance.addPostFrameCallback(
       (_) {
-        providerWish.getAllWishlistProducts(context);
-        homeProvider.getCarousal(context);
+        providerWish.getAllWishlistProducts(
+          context,
+        );
+        providerhome.getCarousal(context);
       },
     );
     return Scaffold(
@@ -53,78 +55,90 @@ class HomePage extends StatelessWidget {
           child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: SingleChildScrollView(
-          child: Consumer<HomeController>(
-            builder: (context, value, child) {
-              return value.isLoading == true
-                  ? const Center(
-                      child: CircularProgressIndicator(),
-                    )
-                  : value.carousal == null
-                      ? const SizedBox()
-                      : Column(
-                          children: [
-                            CarouselSlider.builder(
-                                options: CarouselOptions(
-                                  height: 170.0,
-                                  enlargeCenterPage: true,
-                                  onPageChanged: (position, reason) {},
-                                  enableInfiniteScroll: true,
-                                ),
-                                itemBuilder: (BuildContext context, int index,
-                                    int realIndex) {
-                                  return Container(
-                                      width: MediaQuery.of(context).size.width,
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                              image: NetworkImage(value
-                                                  .carousal!
-                                                  .carousals![index]
-                                                  .image
-                                                  .toString()),
-                                              fit: BoxFit.cover)));
-                                },
-                                itemCount: 3),
-                            const SizedBox(height: 10),
-                            SizedBox(
-                              height: 80,
-                              child: ListView.builder(
-                                  itemCount: value.categoryList.length,
-                                  scrollDirection: Axis.horizontal,
-                                  itemBuilder: ((context, index) {
-                                    return DressButtonWidget(
-                                        index: index,
-                                        image: value.categoryList[index].icon
-                                            .toString(),
-                                        text: value.categoryList[index].category
-                                            .toString());
-                                  })),
-                            ),
-                            const SizedBox(height: 20),
-                            GridView.builder(
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                itemCount: value.products?.length,
-                                gridDelegate:
-                                    const SliverGridDelegateWithFixedCrossAxisCount(
-                                        childAspectRatio: 0.7,
-                                        crossAxisCount: 2,
-                                        mainAxisSpacing: 10,
-                                        crossAxisSpacing: 7),
-                                itemBuilder: ((context, index) => ProductCard(
-                                      product: value.products,
+          child: Column(
+            children: [
+              Consumer<HomeController>(builder: (context, value, child) {
+                return value.isLoading == true
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : Column(
+                        children: [
+                          CarouselSlider.builder(
+                              options: CarouselOptions(
+                                height: 170.0,
+                                enlargeCenterPage: true,
+                                onPageChanged: (position, reason) {},
+                                enableInfiniteScroll: true,
+                              ),
+                              itemBuilder: (BuildContext context, int index,
+                                  int realIndex) {
+                                return value.carousal == null
+                                    ? const SizedBox()
+                                    : Container(
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                                image: NetworkImage(value
+                                                    .carousal!
+                                                    .carousals![index]
+                                                    .image
+                                                    .toString()),
+                                                fit: BoxFit.cover)));
+                              },
+                              itemCount:
+                                  value.carousal?.carousals?.length ?? 0),
+                          SizedBox(
+                            height: 80,
+                            child: ListView.builder(
+                                itemCount: value.categoryList.length,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: ((context, index) {
+                                  return DressButtonWidget(
                                       index: index,
-                                    )))
-                          ],
-                        );
-            },
+                                      image: value.categoryList[index].icon
+                                          .toString(),
+                                      text: value.categoryList[index].category
+                                          .toString());
+                                })),
+                          ),
+                          const SizedBox(height: 20),
+                          // Consumer<HomeController>(builder: (context, value, child) {
+                          //   return
+                          value.isLoading == true
+                              ? const Center(
+                                  child: CircularProgressIndicator(),
+                                )
+                              : value.products == null
+                                  ? const Center(
+                                      child: CircularProgressIndicator(),
+                                    )
+                                  : GridView.builder(
+                                      shrinkWrap: true,
+                                      physics:
+                                          const NeverScrollableScrollPhysics(),
+                                      itemCount: value.products!.length,
+                                      gridDelegate:
+                                          const SliverGridDelegateWithFixedCrossAxisCount(
+                                              childAspectRatio: 0.7,
+                                              crossAxisCount: 2,
+                                              mainAxisSpacing: 10,
+                                              crossAxisSpacing: 7),
+                                      itemBuilder: ((context, index) =>
+                                          ProductCard(
+                                            product: value.products,
+                                            index: index,
+                                          )))
+                        ],
+                      );
+                //   },
+                // ),
+              })
+            ],
           ),
         ),
       )),
     );
   }
-
-  // List<String> pages = [
-  //   'https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  //   'https://images.pexels.com/photos/428338/pexels-photo-428338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  // ];
 }
